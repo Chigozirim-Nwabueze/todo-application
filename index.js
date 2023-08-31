37ebd59caf417bfbfe7e9f8bc2bfa1742a891e9e
Chigozirim-Nwabueze <chigozirimnwabuezeoffice@gmail.com>
2023-08-31 10:51:06 +0100

    TD-13

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
 const addForm = document.querySelector(".add");
-const tasks = document.querySelector(".tasks");
+const tasks = document.querySelector(".task");
 const clearAll = document.querySelector(".clear");
-const messageSpan = document.querySelector(".message span");
-const searchForm = document.querySelector(".search");
+const messageBox = document.querySelector(".message-box span");
+const searchBox = document.querySelector(".search");
 
+window.addEventListener("DOMContentLoaded", (e) => {
+  function updateMessage() {
+    const update = tasks.children.length;
+    messageBox.textContent = `you have ${update} pending tasks`;
+  }
 
-function updateMessage(){
-    const textLength = tasks.children.length;
-    messageSpan.textContent = `You have ${textLength} pending tasks.`;
-}
-updateMessage();
+  updateMessage();
 
+  addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = addForm.task.value.trim();
 
-addForm.addEventListener("submit", event => {
-    event.preventDefault();
-    const value = addForm.task.value.trim()
-
-    if(value.length){
-        tasks.innerHTML += `<li>
-                                <span>${value}</span>
-                                <i class="bi bi-trash-fill delete"></i>
-                            </li>`;
-        addForm.reset();
-        updateMessage();
+    if (value.length) {
+      console.log(value);
+      tasks.innerHTML += `<li>
+                             <span>${value}</span>
+                             <i class="bi bi-trash-fill delete"></i>
+                        </li>`;
+      addForm.reset();
+      updateMessage();
     }
-});
+  });
 
-
-tasks.addEventListener("click", event => {
-    if(event.target.classList.contains("delete")){
-        event.target.parentElement.remove();
-        updateMessage();
+  tasks.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete")) {
+      event.target.parentElement.remove();
+      updateMessage();
     }
-});
+  });
 
-
-clearAll.addEventListener("click", event => {
+  clearAll.addEventListener("click", (e) => {
     const taskItems = tasks.querySelectorAll("li");
-    taskItems.forEach(item => {
-        item.remove();
+
+    taskItems.forEach((item) => {
+      item.remove();
     });
     updateMessage();
-});
+  });
 
-function filterTask(term){
+  function filterTask(term) {
     Array.from(tasks.children)
-    .filter(task => {
+      .filter((task) => {
         return !task.textContent.toLowerCase().includes(term);
-    })
-    .forEach(task => {
+      })
+      .forEach((task) => {
         task.classList.add("hide");
-    });
+      });
 
     Array.from(tasks.children)
-    .filter(task => {
+      .filter((task) => {
         return task.textContent.toLowerCase().includes(term);
-    })
-    .forEach(task => {
+      })
+      .forEach((task) => {
         task.classList.remove("hide");
-    });
+      });
+  }
 
-}
-
-
-searchForm.addEventListener("keyup", event => {
-    const term = searchForm.task.value.trim().toLowerCase();
+  searchBox.addEventListener("keyup", (e) => {
+    const term = searchBox.task.value.trim().toLowerCase();
     filterTask(term);
-});
+  });
 
-
-searchForm.addEventListener("click", event => {
-    if(event.target.classList.contains("reset")){
-        searchForm.reset();
-        const term = searchForm.task.value.trim();
-        filterTask(term);
+  searchBox.addEventListener("click", (e) => {
+    if (e.target.classList.contains("reset")) {
+      searchBox.reset();
+      const term = searchBox.task.value.trim();
+      filterTask(term);
     }
-})
+  });
+});
